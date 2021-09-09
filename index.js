e193d2ab997b6e5cbbf253cc8364c123d2b0cc7b
Youzhang (Mark) Sun <youzhang.mark.sun@gmail.com>
2021-09-09 15:17:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var line = 0;
 var txt = ["class Youzhang_Mark_Sun:", 
             "def __init__(self):", 
             "self.skills = []", 
-            "self.skills.extend([\"Python\", \"Java\", \"MySQL\", \"Algorithms\"])", 
+            "self.skills.extend([\"Java\", \"Python\", \"SQL\", \"Algorithms\"])", 
             "def about_me(self):", 
             "GitHub = \"GitHub\"",
             "Resume = \"Resume\""];
