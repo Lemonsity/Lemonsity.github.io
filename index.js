c0715e89a5b3bd46c9362576b09585684e1f2307
Lemon <38927629+Lemonsity@users.noreply.github.com>
2024-06-28 16:30:00 -0400

    Switch to Haskell syntax, remove annoying typewriter

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-var i = 0;
-var line = 0;
-var txt = ["class Youzhang_Mark_Sun:", 
-            "def __init__(self):", 
-            "self.skills = []", 
-            "self.skills.extend([\"Software Engineering\", \"Functional Programming\", \"Mathematics\"])", 
-            "def about_me(self):", 
-            "GitHub = \"GitHub\"",
-            "Resume = \"Resume\""];
-var gitText = "GitHub"
-var speed = 50;
-var speedSpace = 500;
-var speedLine = 1000;
-var check = true;
-var checkGit = false;
+// var i = 0;
+// var line = 0;
+// var txt = ["class Youzhang_Mark_Sun:", 
+//             "def __init__(self):", 
+//             "self.skills = []", 
+//             "self.skills.extend([\"Software Engineering\", \"Functional Programming\", \"Mathematics\"])", 
+//             "def about_me(self):", 
+//             "GitHub = \"GitHub\"",
+//             "Resume = \"Resume\""];
+// var gitText = "GitHub"
+// var speed = 50;
+// var speedSpace = 500;
+// var speedLine = 1000;
+// var check = true;
+// var checkGit = false;
 
-function typeWriter() {
-    if (check) {
+// function typeWriter() {
+//     if (check) {
         
-        if (i < txt[line].length) {
-            document.getElementById(line + "").innerHTML += txt[line].charAt(i);
-            i++;
-            setTimeout(typeWriter, speed);
-        }
-        if (i == txt[line].length) {
-            i = 0;
-            if (line < 6) {
-                line++;
-            } else {
-                check = false;
-            }
-        }
+//         if (i < txt[line].length) {
+//             document.getElementById(line + "").innerHTML += txt[line].charAt(i);
+//             i++;
+//             setTimeout(typeWriter, speed);
+//         }
+//         if (i == txt[line].length) {
+//             i = 0;
+//             if (line < 6) {
+//                 line++;
+//             } else {
+//                 check = false;
+//             }
+//         }
         
-    }
-}
+//     }
+// }
